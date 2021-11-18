b3c970c04f86b40f5ee3d81310d2512b90fba648
gon1942 <1942kg@gmail.com>
2021-11-18 11:48:04 +0900

    test

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ class GenerateBadges {
 		}
 
 		// If header is in markfdown then make it html
-		const htmlContent = this.mdParser.makeHtml('content');
+		const htmlContent = this.mdParser.makeHtml('<h1></h1>');
 		const {
 			window: {
 				document
